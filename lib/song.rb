bbca31e74954c51c0829955d9d172af466c1dac8
arnie b <arnold.bwaila@gmail.com>
2019-08-30 12:16:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,25 @@
 class Song 
-  #genres artists
+  #name genres artists
+  attr_accessor :name, :artist, :genre
   @@count = 0
-  def initialize
+  @@genres =[]
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
     @@count+=1
+    @@genres << genre
   end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres 
+    if @@count > 0 
+      current = @genre
+      @genre.each do |gnr|
+      
+    
+  
   
